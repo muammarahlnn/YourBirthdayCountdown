ae32af553a48f38b5362517dd46b90edc9f5c1d1
muammarahlnn <21muammarahlan@gmail.com>
2021-09-11 05:01:23 +0800

    validate user inputted date

--- app/src/main/java/com/ardnn/yourbirthdaycountdown/HomeFragment.kt
+++ app/src/main/java/com/ardnn/yourbirthdaycountdown/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.ardnn.yourbirthdaycountdown.databinding.FragmentHomeBinding
 import java.util.*
@@ -16,6 +17,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var currentDate: IntArray
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,11 +26,11 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        // get current date
-        val calendar = Calendar.getInstance()
-        var yearSelected: Int = calendar.get(Calendar.YEAR)
-        var monthSelected: Int = calendar.get(Calendar.MONTH)
-        var daySelected: Int = calendar.get(Calendar.DAY_OF_MONTH)
+        // get current date as initial value for selected date
+        currentDate = getCurrentDate() // [day, month, year]
+        var daySelected = currentDate[0]
+        var monthSelected = currentDate[1]
+        var yearSelected = currentDate[2]
 
         with(binding) {
             etYourDob.setOnClickListener {
@@ -49,7 +51,16 @@ class HomeFragment : Fragment() {
             }
 
             btnSubmit.setOnClickListener { view ->
-                view.findNavController().navigate(R.id.action_homeFragment_to_resultFragment)
+                // check if user born in the future
+                val bornInTheFuture = isUserBornInTheFuture(daySelected, monthSelected, yearSelected)
+                
+                if (bornInTheFuture) {
+                    // tell the user that he/she inputted future date
+                    Toast.makeText(activity, "You are not born yet", Toast.LENGTH_SHORT).show()
+                } else {
+                    // to result section
+                    view.findNavController().navigate(R.id.action_homeFragment_to_resultFragment)
+                }
             }
         }
 
@@ -60,5 +71,35 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getCurrentDate(): IntArray {
+        val calendar = Calendar.getInstance()
+        return intArrayOf(
+            calendar.get(Calendar.DAY_OF_MONTH), // day
+            calendar.get(Calendar.MONTH), // month
+            calendar.get(Calendar.YEAR), // year
+        )
+    }
+
+    private fun isUserBornInTheFuture(day: Int, month: Int, year: Int): Boolean {
+        // first get current date
+        val dayNow = currentDate[0]
+        val monthNow = currentDate[1]
+        val yearNow = currentDate[2]
+
+        // check if born in the future
+        if (year > yearNow) {
+            return true
+        } else if (year == yearNow) {
+            if (month > monthNow) {
+                return true
+            } else if (month == monthNow) {
+                if (day > dayNow) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
